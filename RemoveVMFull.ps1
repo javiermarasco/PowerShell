9f0d8ff4020e8c9c68ffeae61f6bc49459ff1cc3
Antonio Javier Marasco <antonio.marasco@ar.ey.com>
2018-02-20 16:47:32 -0300

    Removes a VM with it's disks and nic. Prepared for managed disks.

---
+++ RemoveVMFull.ps1
@@ -0,0 +1,19 @@
+﻿
+$SubscriptionId = ""
+$ResourceGroupName = ""
+$VMName = ""
+$TenantId = ""
+
+Select-AzureRmSubscription -Subscriptionid $SubscriptionId -TenantId $TenantId
+$VM = Get-AzureRmVM -Name $VMName -ResourceGroupName $ResourceGroupName
+$NICName = $VM.NetworkProfile.NetworkInterfaces.id.Split('/')[8]
+$NICRG = $VM.NetworkProfile.NetworkInterfaces.id.Split('/')[4]
+Remove-AzureRmVM -Name $VM.Name -ResourceGroupName $VM.ResourceGroupName -Force
+Remove-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $NICRG -Force
+foreach ($disk in $VM.StorageProfile.DataDisks){
+    Remove-AzureRmDisk -ResourceGroupName $VM.ResourceGroupName -DiskName $disk.Name -Force
+}
+Remove-AzureRmDisk -ResourceGroupName $VM.ResourceGroupName -DiskName $VM.StorageProfile.OsDisk.Name -Force
+if ($VM.AvailabilitySetReference -ne $null){
+    Remove-AzureRmAvailabilitySet -ResourceGroupName $VM.AvailabilitySetReference.Id.split('/')[4] -Name $VM.AvailabilitySetReference.Id.split('/')[8] -Force
+}
