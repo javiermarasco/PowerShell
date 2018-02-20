cd74b8762fbd8e5502fdcdbd4d93c0c2f6d3589d
Antonio Javier Marasco <antonio.marasco@ar.ey.com>
2018-02-20 16:46:51 -0300

    Script to enable OMS agent in a VM, it updates windows and linux VMs

---
+++ EnableOMS.ps1
@@ -0,0 +1,27 @@
+﻿$SubscriptionId = ""
+$WorkSpaceName = "your workspace name"
+$VMResourceGroup = ""
+$VMName = ""
+
+
+Login-AzureRMAccount
+Select-AzureSubscription -SubscriptionId $SubscriptionId
+$workspace = (Get-AzureRmOperationalInsightsWorkspace).Where({$_.Name -eq $WorkSpaceName})
+
+if ($workspace.Name -ne $WorkSpaceName)
+{
+    Write-Error "Unable to find OMS Workspace $WorkSpaceName. Do you need to run Select-AzureRMSubscription?"
+}
+
+$workspaceId = $workspace.CustomerId
+$workspaceKey = (Get-AzureRmOperationalInsightsWorkspaceSharedKeys -ResourceGroupName $workspace.ResourceGroupName -Name $workspace.Name).PrimarySharedKey
+
+$vm = Get-AzureRmVM -ResourceGroupName $VMResourceGroup -Name $VMName
+$location = $vm.Location
+
+
+if($VMs.StorageProfile.OsDisk.OsType -eq "Linux"){
+    Set-AzureRmVMExtension -ResourceGroupName $VMResourceGroup -VMName $VMName -Name 'OmsAgentForLinux' -Publisher 'Microsoft.EnterpriseCloud.Monitoring' -ExtensionType 'OmsAgentForLinux' -TypeHandlerVersion '1.0' -Location $location -SettingString "{'workspaceId': '$workspaceId'}" -ProtectedSettingString "{'workspaceKey': '$workspaceKey'}"
+}else{
+    Set-AzureRmVMExtension -ResourceGroupName $VMResourceGroup -VMName $VMName -Name 'MicrosoftMonitoringAgent' -Publisher 'Microsoft.EnterpriseCloud.Monitoring' -ExtensionType 'MicrosoftMonitoringAgent' -TypeHandlerVersion '1.0' -Location $location -SettingString "{'workspaceId': '$workspaceId'}" -ProtectedSettingString "{'workspaceKey': '$workspaceKey'}"
+}
